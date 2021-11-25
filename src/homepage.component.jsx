aa5d83c22de0336ada1ee0b654468a8a2bbfee2e
Abrham Getachew <abrhamcisco@gmail.com>
2021-11-25 16:57:22 -0300

    Created project with create-react-app and added files for homepage 2

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './homepage.styles.scss'
 const HomePage = () => (
     <div cassName='homepage'>
-        <div className="dierectory-menu">
+        <div className="directory-menu">
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">HATS</h1>
