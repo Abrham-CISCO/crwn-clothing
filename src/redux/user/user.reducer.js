dce346281ecd974139c6efa9e3c830bac8af6a8a
Abrham Getachew <abrhamcisco@gmail.com>
2021-12-15 03:50:50 -0800

    Redux

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = {
+    currentUser: null
+}
+//default value state = INITIAL_STATE
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case 'SET_CURRENT_USER':
+            return{
+                ...state, currentUser: action.payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
